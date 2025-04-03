337436be8cfc1dde88c2ab5a8fde59d820c4ddaf
ToyaHinata <137118831+RealToya@users.noreply.github.com>
2025-04-03 12:52:13 +0100

    Fifth Exercise Update

---
+++ FifthHTML/script.js
@@ -0,0 +1,26 @@
+let name = 'Bruh';
+
+console.log(name);
+
+let age = 21;
+let multiplication = age * 7;
+
+console.log(multiplication)
+
+let friends = ["Gomez", "Mario", "Cris"]
+
+let friendarray = [{
+    name: "Gomez",
+    age: 21,
+    isStudent: true
+},
+{
+    name: "Mario",
+    age: 21,
+    isStudent: true
+},
+{
+    name: "Cris",
+    age: 21,
+    isStudent: true
+}];
