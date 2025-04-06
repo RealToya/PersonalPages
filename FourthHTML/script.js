1a475234f2f87d58df42bf5c1ab5bd0a85952308
ToyaHinata <137118831+RealToya@users.noreply.github.com>
2025-04-06 16:44:41 +0100

    Update Exercise 4

--- FourthHTML/script.js
+++ FourthHTML/script.js
@@ -1,12 +1,14 @@
 let FaqArray = [];
 let FaqElement = document.querySelectorAll(".faq-item")
 
-FaqElement.forEach((element) => {
-    element.addEventListener("mouseover", () => OpenFAQ(element)); // => é usado pois não estamos a referenciarmos nos a "this" mas a cada instancia da classe na pagina
-});
+FaqElement.forEach((element) => {element.addEventListener("mouseover", () => OpenFAQ(element))}); // => é usado pois não estamos a referenciarmos nos a "this" mas a cada instancia da classe na pagina
 
 function OpenFAQ(faqnum) 
-{
-    console.log(faqnum.innerHTML)
+{    
+    if (faqnum.getElementsByClassName("faqdescription").length > 0) {
+        document.querySelectorAll(".faqdescription").forEach(description => {description.style.display = "none";}); //Dá display none a todos os elementos para depois dar block ao objeto em "Hover"
+
+        faqnum.getElementsByClassName("faqdescription")[0].style.display = "block"; //Utilizando o numero dado por cada elemento aceder ao elemento de display.
+    }
 }
 
