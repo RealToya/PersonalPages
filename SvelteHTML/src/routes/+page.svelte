4bc105e622eacd9a49b156afd21ed134b169c10b
ToyaHinata <shmeerzcomercial@gmail.com>
2025-04-17 23:24:39 +0100

    svelte update

---
+++ SvelteHTML/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="pt">
+    let name = "Pedro";
+    let src = '/favicon.png';
+</script>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+
+<img src="{src}" alt="favicon">
+
+<style>
+    h1
+    {
+        color: pink;
+        font-size: 20px;
+    }
+
+</style>
